76a785f2848ecd49da005f869731eafc6777e0c6
Gabriel Perry <gperry22@gmail.com>
2020-01-07 16:59:00 -0500

    First Upload

---
+++ assets/js/js.js
@@ -0,0 +1,17 @@
+console.log("App Starting")
+
+
+// var rightDiv = $("#rightSide");
+// var img = $("<img id='bgPic'src='../assets/images/one.jpg'>")
+
+// function onHomeLoad() {
+
+//     img.fadeIn(5000)
+//     rightDiv.append(img)
+
+
+// }
+
+// onHomeLoad()
+
+
